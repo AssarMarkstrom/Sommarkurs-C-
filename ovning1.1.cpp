68666c469a351a87cd243f6fa5c98e7a448eba64
assar markström <assarmarkstrom@gmail.com>
2022-06-17 17:21:28 +0200

    initial setup

--- ovning1.1.cpp
+++
@@ -1,7 +0,0 @@
-#include <iostream>
-using namespace std;
-
-int main() {
-    cout << "Hej" << endl;
-    return 0;
-}
---
+++ ovningar/ovning1.1.cpp
@@ -0,0 +1,18 @@
+// First C++ Program
+
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int tal1, tal2, summa;  //Du behöver tre variabler
+ 
+    cout << "Mata in ett tal: ";
+    cin >> tal1;
+    cout << "Mata in ett tal: ";
+    cin >> tal2;
+    summa = tal1 + tal2;
+    cout << "tal1:" << tal1 << endl;
+    cout << "tal2:" << tal2 << endl;
+    cout << "summa:" << summa << endl;
+}
